8057309485f7e01d23568c28148fd9ad6c4cb70d
rslugo9709 <ras.inte@gmail.com>
2024-04-14 00:21:03 -0500

    se corrije problema con el put

--- src/controllers/updates/updateClient.js
+++ src/controllers/updates/updateClient.js
@@ -15,31 +15,13 @@ async function updateClient(req, res){
             
             return res.status(404).json({ message: error.message });
         }
-        //Validamos que no haya llegado nada vacio, en caso tal se coloca la informacion original
-        if(!nombre){
-            nombre = cliente.nombre;
-        }
-        if(!telefono){
-            telefono = cliente.telefono;
-        }
-        if(!direccion){
-            direccion = cliente.direccion;
-        }
-        if(!correo){
-            correo = cliente.correo;
-        }
-        if(!tDocumento){
-            tDocumento = cliente.tDocumento;
-        }
-        if(!nDocumento){
-            nDocumento = cliente.nDocumento;
-        }
-        if(!MPago){
-            MPago = cliente.MPago;
-        }
+        
+        
 
 
-  
+        
+
+        
         await cliente.update({ nombre,tDocumento, nDocumento, telefono, correo, direccion, MPago });
 
 
